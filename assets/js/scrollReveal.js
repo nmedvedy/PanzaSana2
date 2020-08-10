b831d00bff928ded3673f3531247efe83f627e8d
Nicolas Medvedy <nicolasivanm@gmail.com>
2020-08-10 15:23:27 -0300

    images resized and files restructured

--- assets/js/scrollReveal.js
+++ assets/js/scrollReveal.js
@@ -1,37 +1,37 @@
-window.sr = ScrollReveal();
+window.sr = ScrollReveal()
 
 sr.reveal('.header-container', {
     duration: 3000,
     origin: 'bottom',
-    distance: '-100px'
-});
+    distance: '-100px',
+})
 
 sr.reveal('.container', {
     duration: 3000,
     origin: 'bottom',
-    distance: '400px'
-});
+    distance: '400px',
+})
 
 sr.reveal('.recetas', {
     duration: 3000,
     origin: 'left',
-    distance: '400px'
-});
+    distance: '400px',
+})
 
 sr.reveal('.videos', {
     duration: 3000,
     origin: 'bottom',
-    distance: '600px'
-});
+    distance: '600px',
+})
 
 sr.reveal('.instagram', {
     duration: 3000,
     origin: 'right',
-    distance: '400px'
-});
+    distance: '400px',
+})
 
 sr.reveal('#formulario', {
     duration: 3000,
     origin: 'top',
-    distance: '500px'
-});
+    distance: '500px',
+})
